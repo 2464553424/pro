afbdd5a23e8377caf8d3d1fd85843c0c6626310a
2464553424 <2464553424@qq.com>
2018-09-17 14:58:15 +0800

    散开的

---
+++ protet/protet/san/asd.m
@@ -0,0 +1,21 @@
+//
+//  asd.m
+//  protet
+//
+//  Created by Fly on 2018/9/17.
+//  Copyright © 2018年 上海易迪爱网络工程有限公司. All rights reserved.
+//
+
+#import "asd.h"
+
+@implementation asd
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        NSLog(@"三开发");
+    }
+    return self;
+}
+@end
