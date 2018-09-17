f4d114a32e619fa1035caee510e7825a30cb1012
2464553424 <2464553424@qq.com>
2018-09-17 14:59:31 +0800

    sa

---
+++ protet/protet/asd/xzc.m
@@ -0,0 +1,21 @@
+//
+//  xzc.m
+//  protet
+//
+//  Created by Fly on 2018/9/17.
+//  Copyright © 2018年 上海易迪爱网络工程有限公司. All rights reserved.
+//
+
+#import "xzc.h"
+
+@implementation xzc
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
